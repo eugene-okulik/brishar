15c56091819b8c7a1ad7f56b16eb6f8f09bb9643
Yaroslav Brish <yaroslav.brish@gmail.com>
2024-09-27 19:13:32 +0300

    Fix for exercise 3

--- homework/yaroslav_brish/Homework_27/hw27_exercise_3.py
+++ homework/yaroslav_brish/Homework_27/hw27_exercise_3.py
@@ -3,8 +3,9 @@ from playwright.sync_api import Page, expect
 
 def test_color_change(page: Page):
     page.goto('https://demoqa.com/dynamic-properties')
-    clickable_button = page.locator('#enableAfter')
-    expect(clickable_button).to_be_enabled()
+    # clickable_button = page.locator('#enableAfter')
+    # expect(clickable_button).to_be_enabled()
     color_button = page.locator('#colorChange')
+    expect(color_button).to_have_css('color', 'rgb(220, 53, 69)')
     color_button.click()
     expect(color_button).to_be_focused()
